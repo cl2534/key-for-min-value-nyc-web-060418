e055c15df6c26988eab6197bcfd81a5ee53428ae
Changmin Lim <chang.lim92@gmail.com>
2018-05-29 05:41:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  firstkey = name_hash.first[0]
+  firstval = name_hash.first[1]
+  while name_hash.length > 0
+    name_hash.collect do |name, value|
+      if value < firstval 
+        firstval = value
+        firstkey = name
+        
 end
