cdbdfc0f18ff7dff6ee32924e26ed6f8f0ae0e7d
Changmin Lim <chang.lim92@gmail.com>
2018-05-29 05:45:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,14 @@
 def key_for_min_value(name_hash)
   firstkey = name_hash.first[0]
   firstval = name_hash.first[1]
-  while name_hash.length > 0
+  if name_hash.length > 0
     name_hash.each do |name, value|
       if value < firstval 
         firstval = value
         firstkey = name
       end 
     return firstkey 
-    return nil if name_hash.length <= 0 
-end
+  else 
+    return nil 
+  end
 end 
